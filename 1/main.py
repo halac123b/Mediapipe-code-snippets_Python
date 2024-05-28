28b361fd0e22bb3ecc925059652bbd61a2d97dca
halac123b <halac123b@gmail.com>
2024-05-28 17:55:12 +0700

    Add m_1

---
+++ 1/main.py
@@ -0,0 +1,25 @@
+import mediapipe as mp
+import PIL
+import numpy as np
+
+# Read image and convert to Numpy array
+img = PIL.Image.open("test2.png")
+img = img.convert("RGB")
+img = np.array(img)
+
+# FaceMesh detection module of Mediapipe
+## static_image_mode: ảnh tĩnh chứ k phải video
+## refine_landmarks: lọc lại giá trị landmark, tăng độ chính xác nhưng nặng performance, vì dùng ảnh tĩnh nên k cần
+## max_num_faces: số face tối đa cần detect là 1
+## min_detection_confidence: ngưỡng confindence khi detect
+with mp.solutions.face_mesh.FaceMesh(
+    static_image_mode=True,
+    refine_landmarks=False,
+    max_num_faces=1,
+    min_detection_confidence=0.5,
+) as face_mesh:
+    # Bắt đầu chạy nhận diện
+    results = face_mesh.process(img)
+
+print(results.__class__.__name__)
+print(len(results.multi_face_landmarks))
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Mediapipe-code-snippets_Python
+
+<p>Folder list:</p>
+<ol>
+  <li>Face detection</li>
+</ol>
